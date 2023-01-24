8bea9364001d5c537798444b81fcc472744f1325
joy-hui <tongwandou432@gmail.com>
2023-01-24 21:27:29 +0000

    the code1

--- js/logic.js
+++ js/logic.js
@@ -177,6 +177,6 @@ submitButton.addEventListener("click", function () {
   localStorage.setItem("highScores", JSON.stringify(highScoresList));
   // go to highscores page
   window.location.href =
-    "/highscores.html";
+    "/Users/huizhao/bootcamp/Quiz/highscores.html";
   //window.location.href = "/starter/highscores.html";
 });
